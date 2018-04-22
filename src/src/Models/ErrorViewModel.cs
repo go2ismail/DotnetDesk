3ae01cf1ed01f200273ca82154a76a755eb573d3
ismail hamzah <gohamzah@outlook.com>
2018-04-22 19:14:45 +0700

    initial creation using visual studio 2017 community edition

---
+++ src/src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace src.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
