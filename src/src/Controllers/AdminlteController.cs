b462102038452808994a1b2021179b8a17d331e7
ismail hamzah <gohamzah@outlook.com>
2018-04-25 11:51:50 +0700

    add adminlte

---
+++ src/src/Controllers/AdminlteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    public class AdminlteController : Controller
+    {
+        public IActionResult Blank()
+        {
+            return View();
+        }
+    }
+}
